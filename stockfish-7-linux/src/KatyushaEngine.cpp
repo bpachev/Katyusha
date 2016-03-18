7682f0c75dcf0bac60659f09f08b6b7b799d31de
Benjamin Pachev <benjaminpachev@gmail.com>
2016-03-17 18:38:22 -0600

    Fixed a bug that returned the wrong sign of evaluation.

--- stockfish-7-linux/src/KatyushaEngine.cpp
+++ stockfish-7-linux/src/KatyushaEngine.cpp
@@ -3,7 +3,7 @@
 KatyushaNet network;
 string weightsfile = "/home/benjamin/Katyusha/adagrad_weights2.npz";
 int pos_rep_arr[Analyze::NB_FEATURES];
-bool is_active = false;
+bool is_active = true;
 
 bool KatyushaEngine::engine_active()
 {
@@ -29,5 +29,5 @@ Value KatyushaEngine::to_stockfish_value(float raw_eval)
 {
   //TODO: come up with a better transformation
   Value v = VALUE_DRAW;
-  return (v + (int)(raw_eval*50*PawnValueEg));
+  return (v + ((int)(raw_eval*50))*PawnValueEg);
 }
